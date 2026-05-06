78179728b27714257fd0cd034007dd95967080ae
dade150 <davidecruciata@gmail.com>
2026-05-06 10:29:08 +0200

    update bugs

--- src/app/features/accounts/containers/account-movements/account-movements.component.ts
+++ src/app/features/accounts/containers/account-movements/account-movements.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, viewChild } from '@angular/core';
+import { Component, inject, OnInit, signal, viewChild } from '@angular/core';
 import { AccountMovement, MOCK_MOVEMENTS } from '../../models/movement.model';
 import { MovementListComponent } from '../../components/movement-list/movement-list.component';
 import { BankCurrencyPipe } from '../../../../shared/pipes/bank-currency.pipe';
@@ -8,9 +8,13 @@ import { DatePipe } from '@angular/common';
   selector: 'app-account-movements',
   standalone: true,
   imports: [MovementListComponent, BankCurrencyPipe, DatePipe],
+  providers: [DatePipe, BankCurrencyPipe],
   templateUrl: './account-movements.component.html'
 })
 export class AccountMovementsComponent implements OnInit {
+
+  private datePipe = inject(DatePipe);
+  private bankCurrencyPipe = inject(BankCurrencyPipe);
   // State signals
   movements = signal<AccountMovement[]>([]);
   isLoading = signal(false);
@@ -37,4 +41,29 @@ export class AccountMovementsComponent implements OnInit {
     this.movementList()?.resetFilter();
     this.selectedMovement.set(null);
   }
+
+  downloadCsv(): void {
+    console.log("ciao");
+      const header = ['ID', 'Data', 'Descrizione', 'Importo', 'Tipo', 'Categoria'];
+      const rows = this.movementList()?.filteredMovements().map(m => [
+        m.id,
+        this.datePipe.transform(m.date, 'dd/MM/yyyy'),
+        m.description,
+        this.bankCurrencyPipe.transform(m.amount),
+        m.type,
+        m.category
+      ]) || [];
+  
+      const csvContent = [header, ...rows].map(e => e.join(',')).join('\n');
+      const blob = new Blob([csvContent], { type: 'text/csv' });
+      const url = URL.createObjectURL(blob);
+  
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'movements.csv';
+      a.click();
+  
+      URL.revokeObjectURL(url);
+  }
+
 }
